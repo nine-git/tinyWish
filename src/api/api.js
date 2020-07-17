9b6926789c5049a50d77047aea3909ada9f6ad23
xanadu-zxl <xanadu.zxl@gamil.com>
2020-07-17 14:06:37 +0800

    fea：动态传参数

--- src/api/api.js
+++ src/api/api.js
@@ -1,23 +1,16 @@
+/* eslint-disable no-template-curly-in-string */
 import http from '../unit/http'
 // import cookie from 'js-cookie'
 
 export default {
-  // // 工单查询
-  // postFeedBackListAPI (json) {
-  //   return http.post('/mobile/wsq/WP/event/search', json)
-  // },
 
-  // // 进行中
-  // getVoteListSuccessAPI () {
-  //   return http.get('/api/v4/tags/2/taggable_forms', '')
-  // },
   // 获取表单对象
-  getFormsAPI () {
-    return http.get('/api/v4/forms/334', '')
+  getFormsAPI (tableID) {
+    return http.get('/api/v4/forms/${tableID}', '')
   },
   // 获取表单数据
-  getFormsResponsesAPI () {
-    return http.get('/api/v4/forms/334/responses', '')
+  getFormsResponsesAPI (tableID) {
+    return http.get('/api/v4/forms/${tableID}/responses', '')
   }
 
 }
