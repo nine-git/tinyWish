bfcb8dbf1f1c5fcd51f5822e40a2b1767c1e5e4a
xanadu-zxl <xanadu.zxl@gamil.com>
2020-07-17 16:16:49 +0800

    feat：审核数暂停

--- src/api/unit.js
+++ src/api/unit.js
@@ -8,7 +8,7 @@ export default {
       let tableObjData = {}
       let ele = element.mapped_values
       tableObjData.supplies_name = ele.supplies_name.exported_value[0]
-      tableObjData.supplies_img = ele.supplies_img.exported_value[0]
+      tableObjData.supplies_img = ele.supplies_img.exported_value[0].slice(ele.supplies_img.exported_value[0].indexOf('（') + 1, ele.supplies_img.exported_value[0].indexOf('）'))
       tableObjData.supplies_time = ele.supplies_time.exported_value[0]
       tableObjData.review_state = ele.review_state.exported_value[0]
 
