9fe044550f8a6b28ca1ac856d193699f3bafa489
xanadu-zxl <xanadu.zxl@gamil.com>
2020-07-23 11:26:51 +0800

    feat：apply

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,23 +1,32 @@
 export default [
   {
-    path: '/',
-    name: 'home',
-    component: () => import(/* webpackChunkName:'home' */'@/components/view/home')
+    path: "/",
+    name: "home",
+    component: () =>
+      import(/* webpackChunkName:'home' */ "@/components/view/home")
   },
   {
-    path: '/wish',
-    name: 'wish',
-    component: () => import(/* webpackChunkName:'wish' */'@/components/view/wish')
+    path: "/wish",
+    name: "wish",
+    component: () =>
+      import(/* webpackChunkName:'wish' */ "@/components/view/wish")
   },
   {
-    path: '/claim',
-    name: 'claim',
-    component: () => import(/* webpackChunkName:'claim' */'@/components/view/claim')
+    path: "/claim",
+    name: "claim",
+    component: () =>
+      import(/* webpackChunkName:'claim' */ "@/components/view/claim")
   },
   {
-    path: '/donation',
-    name: 'donation',
-    component: () => import(/* webpackChunkName:'donation' */'@/components/view/donation')
+    path: "/apply",
+    name: "apply",
+    component: () =>
+      import(/* webpackChunkName:'apply' */ "@/components/view/apply")
+  },
+  {
+    path: "/donation",
+    name: "donation",
+    component: () =>
+      import(/* webpackChunkName:'donation' */ "@/components/view/donation")
   }
-
-]
+];
